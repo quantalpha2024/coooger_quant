cc2b35ebade0d085ecb400251fb489cd55e3c5a2
quantalpha2024 <quantalpha2024@outlook.com>
2025-08-11 17:41:51 +0800

    test

--- read_clisehouse.py
+++ read_clisehouse.py
@@ -143,7 +143,7 @@ def get_t_d_accountdetail():
     for memberid in MemberID['MemberID']:
             # 第二步：获取实际数据
             data_query = f"""
-                         SELECT *
+                         SELECT CreateTime,AccountDetailID,MemberID,Balance,Source,Amount
                          FROM t_d_accountdetail where MemberID='{memberid}' 
                          """
             result_data = ch_client.execute(data_query)
