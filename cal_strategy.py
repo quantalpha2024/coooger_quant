d745f6ac7185f4cc08d268bc30cc47fe42f79e57
quantalpha2024 <quantalpha2024@outlook.com>
2025-08-29 20:42:41 +0800

    tset

---
+++ cal_strategy.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as mdates
+df=pd.read_excel('策略实盘交易明细.xlsx')
+print(df)
+
+def resample_pl(df, rule="1D"):
+    '''
+    净值曲线
+    '''
+    rule = rule.upper()
+    if rule[-1:] == 'M':
+        rule = rule[:-1] + 'T'
+    agg_rule = {
+        '净值': 'last'
+    }
+    cols = [k for k in agg_rule.keys()]
+    cols.append('时间')
+    temp = df[cols].resample(rule=rule, closed='left', label='left', on='时间').agg(agg_rule)
+    temp['净值'] = temp['净值'].fillna(method='ffill')
+    temp.reset_index(inplace=True)
+    return temp
+def calculate_max_drawdown(df):
+    """计算最大回撤"""
+    peak = df['净值'].cummax()
+    drawdown = (df['净值'] - peak) / peak
+    max_drawdown = drawdown.min()
+    return abs(max_drawdown)
+def calculate_annual_return(df):
+        """计算年化收益率"""
+        start_val = df['净值'].iloc[0]
+        end_val = df['净值'].iloc[-1]
+        days = (df['时间'].iloc[-1] - df['时间'].iloc[0]).days
+        #print(days)
+        annual_return =( (end_val-start_val) / start_val) * (365.25 / days)
+        return annual_return
+def calculate_sharpe_ratio(df, risk_free_rate=0.0):
+        """计算夏普比率"""
+        returns = df['净值'].pct_change().dropna()
+        excess_returns = returns - risk_free_rate / (365*96)  # 假设收益率
+        sharpe = excess_returns.mean() / excess_returns.std() * np.sqrt(365*96)
+        return sharpe
+print("最大回撤:",format(calculate_max_drawdown(df), '.4%') )
+print("年化收益率:",format(calculate_annual_return(df), '.4%') )
+print("卡玛比率:",round(calculate_annual_return(df)/calculate_max_drawdown(df),4))
+df=resample_pl(df,'15m')
+print("夏普率:",round(calculate_sharpe_ratio(df),4))
+
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# 绘制图表
+ax.plot(df['时间'],df['净值'])
+
+# 设置时间格式
+date_format = mdates.DateFormatter('%Y-%m-%d')  # 定义日期格式
+ax.xaxis.set_major_formatter(date_format)
+
+# 自动调整日期标签显示，避免重叠
+fig.autofmt_xdate()
+plt.show()
---
+++ t1.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 使用黑体
+plt.rcParams['axes.unicode_minus'] = False    # 解决负号显示问题
+df=pd.read_excel(r'D:\coinw\data\0114-0828每日成交汇总.xlsx')
+df['日期']=pd.to_datetime(df['日期'])
+print(df.head())
+df['总成交量']=df['总成交量'].cumsum()
+df['BTC成交量']=df['BTC成交量'].cumsum()
+df['ETH成交量']=df['ETH成交量'].cumsum()
+df['其他成交量']=df['其他成交量'].cumsum()
+
+for colum in ['已实现盈利','BTC盈亏',
+              'ETH盈亏','其他盈亏']:
+
+    df[colum]=df[colum].cumsum()
+total_volume = ['总成交量', 'BTC成交量', 'ETH成交量', '其他成交量']
+profit=['已实现盈利','BTC盈亏',
+              'ETH盈亏','其他盈亏']
+for i in range(4):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    # 绘制图表
+    df.plot.scatter(x=total_volume[i], y=profit[i], title='散点图')
+    # 设置时间格式
+    date_format = mdates.DateFormatter('%Y-%m-%d')  # 定义日期格式
+    ax.xaxis.set_major_formatter(date_format)
+    # 自动调整日期标签显示，避免重叠
+    fig.autofmt_xdate()
+    # 计算回归线
+    z = np.polyfit(df[total_volume[i]], df[profit[i]], 1)  # 1表示线性回归
+    p = np.poly1d(z)  # 创建多项式函数
+
+    # 绘制回归线
+    x_line = np.linspace(df[total_volume[i]].min(), df[total_volume[i]].max(), 227)
+    y_line = p(x_line)
+    df['预测']=z[0]*df[total_volume[i]]+z[1]
+    #print(y_line)
+    plt.plot(x_line, y_line, "r--", linewidth=2, label='回归线')
+
+    # 添加方程文本
+    equation = f"y = {z[0]:.8f}x + {z[1]:.2f}"
+    plt.annotate(equation, xy=(0.05, 0.95), xycoords='axes fraction',
+                 fontsize=12, bbox=dict(boxstyle="round,pad=0.3", fc="white", alpha=0.8))
+
+    # 添加R²值
+    from sklearn.metrics import r2_score
+
+    r2 = r2_score(df['预测'], df[profit[i]])
+    print(r2)
+    plt.annotate(f"R方 = {r2:.3f}", xy=(0.05, 0.85), xycoords='axes fraction', fontsize=12, bbox=dict(boxstyle="round,pad=0.3", fc="white", alpha=0.8))
+
+    # 添加图例和网格
+    plt.legend()
+    plt.grid(True, linestyle='--', alpha=0.7)
+
+    plt.tight_layout()
+    plt.show()
+    df['残差']=df[profit[i]]-df['预测']
+    df.plot.scatter(x='总成交量', y='残差')
+    # 设置时间格式
+    date_format = mdates.DateFormatter('%Y-%m-%d')  # 定义日期格式
+    ax.xaxis.set_major_formatter(date_format)
+    # 自动调整日期标签显示，避免重叠
+    fig.autofmt_xdate()
+    plt.show()
+
